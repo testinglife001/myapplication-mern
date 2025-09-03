8c41d92b5b7dbeaaeb5d44bca6ed2bba6f664d43
testinglife001 <testing.life001@gmail.com>
2025-09-03 10:03:47 +0600

    update server.js &  vercel.json again

--- server.js
+++ server.js
@@ -38,13 +38,14 @@ app.use('/rest-api', dashboardRoute);
 app.use('/rest-api', homeRoute);
 app.use('/rest-api', homeCommentRoute);
 
-
+/*
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, "./frontend/dist")))
     app.get('*', (req, res) => {
         res.sendFile(path.resolve(__dirname, './', 'frontend', 'dist', 'index.html'))  
     });
 }
+*/
 
 // ✅ Health check route
 app.get('/', (req, res) => {
